03f628a1466ba7ccccd7758d698c4b1011df2ad1
Nurlan Muslim <muslim39n@gmail.com>
2024-06-28 13:48:41 +0600

    disable cors

--- internal/api/server/server.go
+++ internal/api/server/server.go
@@ -12,6 +12,20 @@ import (
 func Engine() {
 	engine := gin.Default()
 
+	engine.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	})
+
 	routers.SetAdminRoutesV1(engine)
 
 	err := engine.Run(fmt.Sprintf(":%s", os.Getenv("HTTP_PORT")))
